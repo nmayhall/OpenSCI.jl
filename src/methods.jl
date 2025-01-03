bbd579fd6468d27fce5ecac4cc750dd5caef8761
Nick Mayhall <nmayhall@vt.edu>
2025-01-03 13:46:59 -0500

    need to finish applying jump operators

--- src/methods.jl
+++ src/methods.jl
@@ -3,6 +3,8 @@ using OpenSCI
 
 function LinearAlgebra.rmul!(dρ::DyadSum{N,T}, L::Lindbladian{N}, ρ::DyadSum{N,T}, thresh=1e-16) where {N,T} 
     empty!(dρ)
+
+    # Unitary part
     for (pauli,pcoeff) in L.H.ops
        
         for (dyad,dcoeff) in ρ
@@ -17,18 +19,46 @@ function LinearAlgebra.rmul!(dρ::DyadSum{N,T}, L::Lindbladian{N}, ρ::DyadSum{N
             sum!(dρ, -coeff*sd.dyad)
         end
     end
+
+    # Non-unitary
+    for i in 1:length(γ)
+        for (pauli,pcoeff) in L.L.ops
+            for (dyad,dcoeff) in ρ
+                sd = pauli * dyad
+                coeff = sd.coeff * pcoeff * dcoeff
+                abs2(coeff) > thresh || continue
+                sum!(dρ, coeff*sd.dyad)
+                
+                sd = dyad * pauli
+                coeff = sd.coeff * pcoeff * dcoeff
+                abs2(coeff) > thresh || continue
+                sum!(dρ, -coeff*sd.dyad)
+            end
+        end
+    end
+
     return
 end
 
 function Base.:*(L::Lindbladian{N}, ρ::DyadSum{N,T}) where {N,T}
-    out = DyadSum(N,T=T)
-    rmul!(out, L, ρ)
-    return out
+    # out = DyadSum(N,T=T)
+    # rmul!(out, L, ρ)
+    dρ = L.H*ρ - ρ*L.H
+    return dρ 
 end
-function Base.:*(L::Lindbladian{N}, ρ::Dyad{N}, T=ComplexF64) where N
-    out = DyadSum(N,T=T)
-    rmul!(out, L, DyadSum(ρ, T=T))
-    return out
+function Base.:*(L::Lindbladian{N}, ρd::Dyad{N}, T=ComplexF64) where N
+    # out = DyadSum(N,T=T)
+    # rmul!(out, L, DyadSum(ρ, T=T))
+    ρ = DyadSum(ρd, T=T)
+    dρ = L.H*ρ - ρ*L.H
+    
+    for i in 1:length(L.γ)
+        Li = L.L[i]
+        dρ += L.γ[i] * (Li * (dρ * Li'))
+        LL = Li * Li'
+        dρ -= 0.5*L.γ[i]*(LL*ρd + ρd*LL)
+    end
+    return dρ 
 end
 
 Base.vec(d::Dyad{N}) where N = 1 + d.ket.v + d.bra.v*(BigInt(2)^N)
--- test/tests1.jl
+++ test/tests1.jl
@@ -2,13 +2,14 @@ using PauliOperators
 using OpenSCI
 using Test
 using LinearAlgebra
+using Printf
 
 @testset "tests1.jl" begin
 
-    N = 6
+    N = 2
 
-    @test vec(Dyad(N, 4, 8)) == 517 
-    Λ = rand(Lindbladian{N})
+    @test vec(Dyad(6, 4, 8)) == 517 
+    Λ = rand(Lindbladian{N}, nH=100, nL=0)
     display(Λ)
 
     ρ = DyadSum(Dyad(N,0,0)) 
@@ -27,9 +28,27 @@ using LinearAlgebra
     
     err = Matrix(Λ)*vec(Matrix(ρ)) - vec(Matrix(Λ*ρ))
     @test isapprox(norm(err), 0, atol=1e-14)
-   
+    
     err = vec(Matrix(Λ.H)*Matrix(ρ) - Matrix(ρ)*Matrix(Λ.H)) - vec(Matrix(Λ*ρ))
     @test isapprox(norm(err), 0, atol=1e-14)
 
+    U,s,V = svd(Lv)
+    for i in 1:length(s)
+        @printf(" %4i %12.8f %12.8fi\n", i, real(s[i]), imag(s[i]))
+    end
+
+    
+    Λ = rand(Lindbladian{N}, nH=100, nL=0)
+
+    display(Λ)
+    F = eigen(Matrix(Λ.H))
+    ρ = zeros(2^N, 2^N)
+    for i in 1:length(F.values)
+        ρ += randn()*F.vectors * F.vectors'
+    end
+    ρ = ρ/tr(ρ)
+    # display(ρ)
+
+    @show norm(Matrix(Λ)*vec(Matrix(ρ)))
     
 end
