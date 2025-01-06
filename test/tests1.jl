658046c2b56f5dd1e096a1516dfcc6d526e5cb76
Nick Mayhall <nmayhall@vt.edu>
2025-01-06 12:35:47 -0500

    trying to sort out why random state goes to zero

--- test/tests1.jl
+++ test/tests1.jl
@@ -6,7 +6,7 @@ using Printf
 using Random
 
 # @testset "tests1.jl" begin
-function run()
+function run1()
     N = 2
 
     @test vec(Dyad(6, 4, 8)) == 517 
@@ -55,17 +55,19 @@ function run()
     @show norm(Matrix(Λ)*vec(Matrix(ρ)))
    
     # return 
+end
+
+function run2()
 
     #### Now with non-unitary part 
     N = 2
-    Λ = rand(Lindbladian{N}, nH=100, nL=10)
+    Λ = rand(Lindbladian{N}, nH=100, nL=1)
     display(Λ)
     @show ρ = DyadSum(Dyad(N,0,0)) 
 
     err = Matrix(Λ)*vec(Matrix(ρ)) - vec(Matrix(Λ*ρ))
     @test isapprox(norm(err), 0, atol=1e-14)
 
-    println("\n Singular Values of Λ")
     U,s,V = svd(Matrix(Λ))
     
     println("\n Singular Values of Λ")
@@ -81,21 +83,22 @@ function run()
     @printf(" tr(ρss) = %12.8f %12.8fi\n", real(tr(ρss)), imag(tr(ρss)))
 
     Random.seed!(2)
-    tmp = Matrix{Float64}(I, 2^N, 2^N)
+    ρ = Matrix{Float64}(I, 2^N, 2^N)
     
     F = svd(rand(2^N, 2^N))
-    tmp = F.U * Diagonal(F.S) * F.U'
+    ρ = F.U * Diagonal(F.S) * F.U' / sum(F.S)
 
-    tmp /= tr(tmp) 
-    println(" positive? ", eigvals(tmp))
-    println("\n Trace of ρ")
-    display(tr(tmp))
-    display(tmp)
-    tmp = Matrix(Λ)*vec(tmp)
-    tmp = reshape(tmp, (2^N, 2^N))
-    println("\n Trace of Λρ")
-    display(tr(tmp))
-    display(tmp)
+    println(" Eigenvalues of ρ ", eigvals(ρ))
+    println("\n Trace of ρ: ", tr(ρ))
+    println(" ρ")
+    display(ρ)
+    Λρ = Matrix(Λ)*vec(ρ)
+    Λρ = reshape(Λρ, (2^N, 2^N))
+
+    println(" Λρ")
+    display(Λρ)
+    println("\n Trace of Λρ: ", tr(Λρ))
+    println(" Eigenvalues of ρ ", eigvals(Λρ))
 
     println("\n  Do my jump operators sum to I?")
     tmp = PauliSum(N) 
@@ -104,4 +107,6 @@ function run()
     end
     display(tmp)
 end
-run()
+
+run1()
+run2()
