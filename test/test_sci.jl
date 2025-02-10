a23f65a0cdc594411ec28321f9ce27dfb4606091
Nick Mayhall <nmayhall@vt.edu>
2025-02-10 16:47:08 -0500

    heis

--- test/test_sci.jl
+++ test/test_sci.jl
@@ -66,23 +66,13 @@ function test1()
     @test norm(Matrix(tmp1) - Matrix(fill!(tmp1,Matrix(tmp1)))) < 1e-14
 
     println()
-    display(L*v0)
+    # display(L*v0)
 
     err = Matrix(L)*todense(v0) - todense(L*v0)
     @test isapprox(norm(err), 0, atol=1e-14)
 
     selected_ci(L, v0, max_iter_outer=5)
 
-    # v0 = multiply(L, v0)
-    # v0 = multiply(L, v0)
-    # v0 = multiply(L, v0)
-    # Lmat = build_subspace_L(L, v0)
-    # _,s,V = svd(Lmat)
-    # @printf("\n Singular values of Lmat:\n")
-    # for i in 1:length(s)
-    #     @printf(" %4i % 12.8f\n", i, s[i])
-    # end
-  
     Lmat = Matrix(L)
     l = eigvals(Lmat)
     @printf("\n Eigenvalues of Lmat:\n")
